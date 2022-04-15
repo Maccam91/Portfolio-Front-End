c15d126634f3bf1f815b86d074af0ccd35b1fdcc
maccam91 <michael.cortezjr@gmail.com>
2022-04-15 16:30:28 -0500

    added resume and finished mail contact form

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,8 +1,28 @@
 import "./contact.css"
 import Email from "../img/newemail.svg"
 import Resume from "../img/resume.svg"
+import MyResume from "../data/portresume.pdf"
+import { useRef, useState } from "react"
+import emailjs from "emailjs-com"
 
 const Contact = () => {
+    const resumeClick = (e) => {
+        
+    }
+    const formRef =useRef()
+    const [done, setDone] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_m4onkkm', 'template_ro70rrk', formRef.current, 'T_VUN_-U39mHZ3VjL')
+      .then((result) => {
+          console.log(result.text);
+          setDone(true)
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+    }
     return (
         <div className="c">
             <div className="c-bg"></div>
@@ -10,11 +30,11 @@ const Contact = () => {
                 <div className="c-left">
                     <h1 className="c-title"> Contact Info</h1>
                     <div className="c-info-item">
-                        <img
+                        <a className="r-download" href={MyResume} download="portresume.pdf"><img
                         src={Resume}
                         alt=""
                         className="c-icon"
-                        />Resume
+                        /></a>My Resume
                     </div>
                     <div className="c-info-item">
                         <img
@@ -27,8 +47,16 @@ const Contact = () => {
                 </div>
                 <div className="c-right">
                     <div className="c-right-desc">
-                    Let's talk about your project or just chat. I am always up for the challenge and willing to learn or listen.
+                    Let's talk about your project - challenges.<br/> I look forward to learning more from and about you.
                     </div>
+                    <form ref={formRef} onSubmit={handleSubmit}>
+                        <input type="text" placeholder="Your Name" name="user_name"/>
+                        <input type="text" placeholder="Subject" name="user_subject"/>
+                        <input type="text" placeholder="Your Email" name="user_email"/>
+                        <textarea rows="5" placeholder="Message" name="message"/>
+                        <button>Submit</button>
+                        <br/><br/><div className="e-sent">{done && "Thank you I will get in touch with you as soon as I can."}</div>
+                    </form>
                 </div>
             </div>
         </div>
--- src/components/productList/productList.jsx
+++ src/components/productList/productList.jsx
@@ -6,7 +6,7 @@ const ProductList = () => {
     return (
         <div className="pl">
             <div className="pl-texts">
-            <h1 className="pl-title"> Some Projects I've created</h1>
+            <h1 className="pl-title"> Some Projects I've worked on.</h1>
             <p className="pl-desc">
                 These projects are just the beginning of my journey in this career transition and as I grow so will my portfolio.
             </p>
