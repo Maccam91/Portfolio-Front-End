da996c1e71f1c1fc3b47a46d781debacfb16c778
maccam91 <michael.cortezjr@gmail.com>
2022-04-15 16:42:45 -0500

    locally stored gifs in publics folder

--- src/components/data.js
+++ src/components/data.js
@@ -1,22 +1,24 @@
+
+
 export const products = [
     {
         id:1,
-        img:"https://gyazo.com/b662cec27502ba7180d0313f1221a168.gif",
+        img:"images/typewriters.gif",
         link:"https://maccam91.github.io/GAP1/",
     },
     {
         id:2,
-        img:"https://gyazo.com/1b99685757908d8f398c7532265e12b9.gif",
+        img:"images/easypeasy.gif",
         link:"https://polar-garden-31604.herokuapp.com/easypeasy",
     },
     {
         id:3,
-        img:"https://gyazo.com/792f13a77ac874a200e82ed30e8eae44.gif",
+        img:"images/fireside.gif",
         link:"https://agitated-nobel-f91563.netlify.app/",
     },
     {
         id:4,
-        img:"https://i.gyazo.com/00175866f5c9b60f13d97e2893a91491.gif",
+        img:"images/typescript.gif",
         link:"https://6256080a33e3661a3b6f9359--playful-clafoutis-032b36.netlify.app/",
     },
     
